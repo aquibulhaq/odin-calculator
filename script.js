cd71a5b43b6faf6182c97369dba273a2446cf701
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2024-10-27 19:29:19 +0600

    feat: create format function

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+const DISPLAY_WIDTH = 10;
+
+const formatter = Intl.NumberFormat('en-US', {
+  useGrouping: false,
+  maximumFractionDigits: 7,
+  maximumSignificantDigits: 8,
+  roundingPriority: 'lessPrecision',
+  signDisplay: 'negative',
+});
+
+function format(n) {
+  if (!isFinite(n))
+    return 'ERROR';
+
+  const formatted = formatter.format(n);
+  if (formatted.length > DISPLAY_WIDTH)
+    return 'ERROR';
+
+  return formatted;
+}
